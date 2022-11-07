4bc47b72cc3e4bec37e9b14cb82f4902b92787f0
MarkEezZ <goog55776@gmail.com>
2022-11-07 16:05:35 +0300

    migrations added

---
+++ InnoGotchi.API/Migrations/20221107115151_DatabaseCreation.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InnoGotchi.API.Migrations
+{
+    public partial class DatabaseCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bodies",
+                columns: table => new
+                {
+                    BodyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bodies", x => x.BodyId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Eyes",
+                columns: table => new
+                {
+                    EyesId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eyes", x => x.EyesId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Farms",
+                columns: table => new
+                {
+                    FarmId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Farms", x => x.FarmId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mouthes",
+                columns: table => new
+                {
+                    MouthId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mouthes", x => x.MouthId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Noses",
+                columns: table => new
+                {
+                    NoseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Noses", x => x.NoseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    SettingsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AvatarFileName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ActivityStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsInGame = table.Column<bool>(type: "bit", nullable: false),
+                    IsMusic = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.SettingsId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    PetId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    BodyId = table.Column<int>(type: "int", nullable: false),
+                    EyesId = table.Column<int>(type: "int", nullable: false),
+                    NoseId = table.Column<int>(type: "int", nullable: false),
+                    MouthId = table.Column<int>(type: "int", nullable: false),
+                    FarmId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.PetId);
+                    table.ForeignKey(
+                        name: "FK_Pets_Bodies_BodyId",
+                        column: x => x.BodyId,
+                        principalTable: "Bodies",
+                        principalColumn: "BodyId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pets_Eyes_EyesId",
+                        column: x => x.EyesId,
+                        principalTable: "Eyes",
+                        principalColumn: "EyesId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pets_Farms_FarmId",
+                        column: x => x.FarmId,
+                        principalTable: "Farms",
+                        principalColumn: "FarmId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pets_Mouthes_MouthId",
+                        column: x => x.MouthId,
+                        principalTable: "Mouthes",
+                        principalColumn: "MouthId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Pets_Noses_NoseId",
+                        column: x => x.NoseId,
+                        principalTable: "Noses",
+                        principalColumn: "NoseId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    LastEntry = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastExit = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OwnFarm = table.Column<int>(type: "int", nullable: false),
+                    SettingsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Users_Farms_OwnFarm",
+                        column: x => x.OwnFarm,
+                        principalTable: "Farms",
+                        principalColumn: "FarmId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_Settings_SettingsId",
+                        column: x => x.SettingsId,
+                        principalTable: "Settings",
+                        principalColumn: "SettingsId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    GuestsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    FarmId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => x.GuestsId);
+                    table.ForeignKey(
+                        name: "FK_Guests_Farms_FarmId",
+                        column: x => x.FarmId,
+                        principalTable: "Farms",
+                        principalColumn: "FarmId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Guests_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guests_FarmId",
+                table: "Guests",
+                column: "FarmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guests_UserId",
+                table: "Guests",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_BodyId",
+                table: "Pets",
+                column: "BodyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_EyesId",
+                table: "Pets",
+                column: "EyesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_FarmId",
+                table: "Pets",
+                column: "FarmId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_MouthId",
+                table: "Pets",
+                column: "MouthId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_NoseId",
+                table: "Pets",
+                column: "NoseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_OwnFarm",
+                table: "Users",
+                column: "OwnFarm");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_SettingsId",
+                table: "Users",
+                column: "SettingsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guests");
+
+            migrationBuilder.DropTable(
+                name: "Pets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Bodies");
+
+            migrationBuilder.DropTable(
+                name: "Eyes");
+
+            migrationBuilder.DropTable(
+                name: "Mouthes");
+
+            migrationBuilder.DropTable(
+                name: "Noses");
+
+            migrationBuilder.DropTable(
+                name: "Farms");
+
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
